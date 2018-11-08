526705bb6b8060432332946c367b10df96faa8a2
fchange <1787003204@qq.com>
2018-11-08 19:18:05 +0800

    update

--- source/photo/index.md
+++ source/photo/index.md
@@ -2,12 +2,12 @@
 title: 相册
 noDate: 'true'
 ---
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js"></script>
 <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery_lazyload/1.9.7/jquery.lazyload.min.js"></script> -->
-<script src="https://unpkg.com/minigrid@3.1.1/dist/minigrid.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/minigrid@3.1.1/dist/minigrid.min.js"></script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js"></script>
-<link href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/@fancyapps/fancybox@3.5.0/dist/jquery.fancybox.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/@fancyapps/fancybox@3.5.0/dist/jquery.fancybox.min.css" rel="stylesheet">
 
 <div class="ImageGrid"></div>
 
--- tool.js
+++ tool.js
@@ -5,27 +5,34 @@
     const outputfile = "./source/Images/output.json";
     var dimensions;
 
+    fs.exists(outputfile, function (exists) {
+      if(exists) fs.unlink(outputfile, e => {});
+    });
     fs.readdir(path, function (err, files) {
         if (err) {
             return;
         }
         let arr = [];
-        (function iterator(index) {
-            if (index == files.length) {
-                fs.writeFile(outputfile, JSON.stringify(arr, null, "\t"));
-                return;
-            }
 
-            fs.stat(path + "/" + files[index], function (err, stats) {
+        files.forEach((fileName) => {
+            fs.stat(path + "/" + fileName, function (err, stats) {
                 if (err) {
                     return;
                 }
                 if (stats.isFile()) {
-                    dimensions = sizeOf(path + "/" + files[index]);
-                    console.log(dimensions.width, dimensions.height);
-                    arr.push(dimensions.width + '.' + dimensions.height + ' ' + files[index]);
+                    dimensions = sizeOf(path + "/" + fileName);
+                    // console.log(dimensions.width, dimensions.height);
+                    arr.push(dimensions.width + '.' + dimensions.height + ' ' + fileName);
+                    count();
                 }
-                iterator(index + 1);
             })
-        }(0));
+        })
+
+        var countNum = 0;
+        var count = function() {
+            countNum++;
+            if(countNum == files.length) {
+                fs.writeFile(outputfile, JSON.stringify(arr, null, "\t"), e => console.log("count over", e));
+            }
+        }
     });
